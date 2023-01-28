b307ec968f26bac7396c451cf07e8dcea91d9632
sagar <srutisagar7002@gmail.com>
2023-01-28 08:28:28 +0530

    Add a new file to export date

--- app.js
+++ app.js
@@ -1,35 +1,43 @@
 const express = require("express");
 const bp = require("body-parser");
+const date = require(__dirname + "/date.js");
+console.log(date());
 const app = express();
 app.set('view engine', 'ejs');
-app.use(bp.urlencoded({extended: true}));
+app.use(bp.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
 let items = [];
 let witems = [];
 
-app.get("/",function(req,res){
- let today = new Date();
- let options = { weekday: 'long',month: 'long', day: 'numeric' };
- let day = today.toLocaleDateString("en-US", options);
-res.render("list",{Title:day,newitem:items});
+app.get("/", function(req, res) {
+  day = date();
+  res.render("list", {
+    Title: day,
+    newitem: items
+  });
 })
 
-app.post("/",function(req,res){
-   let item = req.body.new;
-   if(req.body.list === "Work"){
-     witems.push(item);
-     res.redirect("/work");
-   }else{
-     items.push(item);
-     res.redirect("/");
-   }
+app.post("/", function(req, res) {
+  let item = req.body.new;
+  if (req.body.list === "Work") {
+    witems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
 })
 
-app.get("/work",function(req,res){
-  res.render("list",{Title:"Work list",newitem:witems});
+app.get("/work", function(req, res) {
+  res.render("list", {
+    Title: "Work list",
+    newitem: witems
+  });
 })
 
-app.listen(process.env.PORT || 3000,function(){
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server is running on port 3000");
 })
